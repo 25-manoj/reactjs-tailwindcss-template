6a6e301d3a31360866ab0410cc76d63aaf462463
25-manoj <125800336+25-manoj@users.noreply.github.com>
2024-09-02 15:42:29 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center bold">
+      <h1 className="underline">Vite + React + Tailwindcss</h1>
+    </div>
+  );
+}
+
+export default App;
